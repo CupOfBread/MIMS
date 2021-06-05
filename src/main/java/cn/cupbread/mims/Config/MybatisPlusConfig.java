92a45394b101930749be099a8e71133e373f89bb
CupOfBread <508087823@qq.com>
2021-06-05 13:25:38 +0800

    ✨新增：Mybatis-Plus配置类

---
+++ src/main/java/cn/cupbread/mims/Config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package cn.cupbread.mims.Config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author : Cup Of Bread
+ * @version : 1.0.0
+ * @date : 2021/6/5
+ * @description :
+ */
+
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
